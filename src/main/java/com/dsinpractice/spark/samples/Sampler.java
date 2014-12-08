88b25ac3d41f6d7b4aa05a3fb6eaeca9e66c0407
Hemanth Yamijala <yhemanth@gmail.com>
2014-12-08 23:27:42 +0530

    Added a sample to demo the sample function in spark RDD

---
+++ src/main/java/com/dsinpractice/spark/samples/Sampler.java
@@ -0,0 +1,27 @@
+package com.dsinpractice.spark.samples;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+
+import java.io.Serializable;
+
+public class Sampler implements Serializable {
+
+    public static void main(String[] args) {
+        if (args.length < 3) {
+            System.out.println("Usage: " + Sampler.class.getName() + " <inpath> <outpath> <samplesize>");
+            System.exit(-1);
+        }
+        Sampler sampler = new Sampler();
+        sampler.run(args);
+    }
+
+    private void run(String[] args) {
+        SparkConf samplerConf = new SparkConf().setAppName("Sampler");
+        JavaSparkContext javaSparkContext = new JavaSparkContext(samplerConf);
+        JavaRDD<String> lines = javaSparkContext.textFile(args[0]);
+        JavaRDD<String> sample = lines.sample(false, Double.parseDouble(args[2]));
+        sample.saveAsTextFile(args[1]);
+    }
+}
