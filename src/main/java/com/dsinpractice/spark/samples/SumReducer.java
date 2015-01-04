8d6dad2514d3ef49b77da585d9b9d332e16b8839
Hemanth Yamijala <yhemanth@gmail.com>
2015-01-04 19:09:30 +0530

    Added a sample for demonstrating summing up by key using reduceByKey

---
+++ src/main/java/com/dsinpractice/spark/samples/SumReducer.java
@@ -0,0 +1,51 @@
+package com.dsinpractice.spark.samples;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.Function2;
+import org.apache.spark.api.java.function.PairFlatMapFunction;
+import scala.Tuple2;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SumReducer implements Serializable {
+
+    public static void main(String[] args) {
+        if (args.length < 2) {
+            System.out.println("Usage: java " + SumReducer.class.getName() + " <input> <output>");
+            System.exit(-1);
+        }
+
+        SumReducer sumReducer = new SumReducer();
+        sumReducer.run(args);
+    }
+
+    private void run(String[] args) {
+        SparkConf aggregatorConf = new SparkConf().setAppName("Sum Aggregator");
+        JavaSparkContext javaSparkContext = new JavaSparkContext(aggregatorConf);
+        JavaRDD<String> lines = javaSparkContext.textFile(args[0]);
+        JavaPairRDD<String, Integer> wordCounts =
+                lines.flatMapToPair(new PairFlatMapFunction<String, String, Integer>() {
+            @Override
+            public Iterable<Tuple2<String, Integer>> call(String line) throws Exception {
+                List<Tuple2<String, Integer>> wordCounts = new ArrayList<Tuple2<String, Integer>>();
+                String[] words = line.split(" ");
+                for (String word : words) {
+                    wordCounts.add(new Tuple2<String, Integer>(word, 1));
+                }
+                return wordCounts;
+            }
+        });
+        JavaPairRDD<String, Integer> finalCounts = wordCounts.reduceByKey(new Function2<Integer, Integer, Integer>() {
+            @Override
+            public Integer call(Integer i1, Integer i2) throws Exception {
+                return i1 + i2;
+            }
+        });
+        finalCounts.saveAsTextFile(args[1]);
+    }
+}
