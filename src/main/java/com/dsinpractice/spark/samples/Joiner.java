4b74e50b2d801b983154984e9c3a441ebbaaa1c0
Hemanth Yamijala <yhemanth@gmail.com>
2015-04-06 22:28:42 +0530

    Modified join / co-group example to a more appropriate one, where it is easier to compare join and cogroup

--- src/main/java/com/dsinpractice/spark/samples/Joiner.java
+++ src/main/java/com/dsinpractice/spark/samples/Joiner.java
@@ -1,5 +1,6 @@
 package com.dsinpractice.spark.samples;
 
+import com.google.common.collect.Ordering;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
@@ -12,6 +13,7 @@ import scala.Tuple2;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Joiner implements Serializable {
@@ -19,7 +21,7 @@ public class Joiner implements Serializable {
     public static void main(String[] args) {
         if (args.length < 4) {
             System.out.println("Usage: " + Joiner.class.getName() +
-                    " <city-file> <language-file> <output> <operation = join | cogroup>");
+                    " <max-temps-file> <min-temps-file> <output> <operation = join | cogroup>");
             System.out.println("Input files to use at resources/join-cogroup.");
             System.exit(-1);
         }
@@ -30,78 +32,70 @@ public class Joiner implements Serializable {
     private void run(String[] args) {
         SparkConf sparkConf = new SparkConf().setAppName("Joiner");
         JavaSparkContext javaSparkContext = new JavaSparkContext(sparkConf);
-        JavaRDD<String> cityFile = javaSparkContext.textFile(args[0]);
-        JavaPairRDD<String, String> cities = selectCities(cityFile);
-        JavaRDD<String> languageFile = javaSparkContext.textFile(args[1]);
-        JavaPairRDD<String, String> languages = selectLanguages(languageFile);
+        JavaPairRDD<String, Double> maxTemps = selectCities(javaSparkContext.textFile(args[0]));
+        JavaPairRDD<String, Double> minTemps = selectCities(javaSparkContext.textFile(args[1]));
         if (args[3].equals("join")) {
-            JavaPairRDD<String, Tuple2<String, String>> cityDetails = cities.join(languages);
-            JavaPairRDD<String, String> countryLanguages = languagesByCountry(cityDetails);
-            countryLanguages.saveAsTextFile(args[2]);
+            handleJoin(args[2], maxTemps, minTemps);
         } else if (args[3].equals("cogroup")) {
-            JavaPairRDD<String, Tuple2<Iterable<String>, Iterable<String>>> cityDetails
-                    = cities.cogroup(languages);
-            JavaPairRDD<String, String> countryLanguages = cogroupedLanguagesByCountry(cityDetails);
-            countryLanguages.saveAsTextFile(args[2]);
+            handleCoGroup(args[2], maxTemps.cogroup(minTemps));
         }
     }
 
-    private JavaPairRDD<String, String> cogroupedLanguagesByCountry(
-            JavaPairRDD<String, Tuple2<Iterable<String>, Iterable<String>>> cityDetails) {
-        return cityDetails.flatMapToPair(
-                new PairFlatMapFunction<Tuple2<String, Tuple2<Iterable<String>, Iterable<String>>>, String, String>() {
-            @Override
-            public Iterable<Tuple2<String, String>> call(
-                    Tuple2<String, Tuple2<Iterable<String>, Iterable<String>>> cityDetail) throws Exception {
-                Tuple2<Iterable<String>, Iterable<String>> countryLanguagesList = cityDetail._2();
-                String country = countryLanguagesList._1().iterator().next();
-                Iterable<String> languages = countryLanguagesList._2();
-                List<Tuple2<String, String>> countryLanguageTuples = new ArrayList<Tuple2<String, String>>();
-                for (String language : languages) {
-                    countryLanguageTuples.add(new Tuple2<String, String>(country, language));
-                }
-                return countryLanguageTuples;
-            }
-        }).reduceByKey(concat());
+    private void handleCoGroup(String outputFile,
+                               JavaPairRDD<String, Tuple2<Iterable<Double>, Iterable<Double>>> cityTempLists) {
+        JavaPairRDD<String, Tuple2<Double, Double>> cityTemps =
+                cityTempLists.mapToPair(new PairFunction<Tuple2<String, Tuple2<Iterable<Double>, Iterable<Double>>>,
+                        String, Tuple2<Double, Double>>() {
+                    @Override
+                    public Tuple2<String, Tuple2<Double, Double>> call(Tuple2<String,
+                            Tuple2<Iterable<Double>, Iterable<Double>>> cityTemps) throws Exception {
+                        Iterable<Double> maxTempsForCity = cityTemps._2()._1();
+                        Iterable<Double> minTempsForCity = cityTemps._2()._2();
+                        Double maxTemp = Double.MIN_VALUE;
+                        for (Double temp : maxTempsForCity) {
+                            if (temp > maxTemp) {
+                                maxTemp = temp;
+                            }
+                        }
+                        Double minTemp = Double.MAX_VALUE;
+                        for (Double temp : minTempsForCity) {
+                            if (temp < minTemp) {
+                                minTemp = temp;
+                            }
+                        }
+                        return new Tuple2<String, Tuple2<Double, Double>>(cityTemps._1(),
+                                new Tuple2<Double, Double>(maxTemp, minTemp));
+                    }
+                });
+        cityTemps.saveAsTextFile(outputFile);
     }
 
-    private JavaPairRDD<String, String> languagesByCountry(JavaPairRDD<String, Tuple2<String, String>> cityDetails) {
-        return cityDetails.mapToPair(new PairFunction<Tuple2<String, Tuple2<String, String>>, String, String>() {
+    private void handleJoin(String outputFile,
+                            JavaPairRDD<String, Double> maxTemps, JavaPairRDD<String, Double> minTemps) {
+        JavaPairRDD<String, Double> maxTempsByCity = maxTemps.reduceByKey(new Function2<Double, Double, Double>() {
             @Override
-            public Tuple2<String, String> call(Tuple2<String, Tuple2<String, String>> countryLanguage) throws Exception {
-                return countryLanguage._2();
-            }
-        }).reduceByKey(concat());
-    }
-
-    private Function2<String, String, String> concat() {
-        return new Function2<String, String, String>() {
-            @Override
-            public String call(String s1, String s2) throws Exception {
-                return s1 + "," + s2;
-            }
-        };
-    }
-
-    private JavaPairRDD<String, String> selectLanguages(JavaRDD<String> languageFile) {
-        JavaPairRDD<String, String> cityLanguagePairs = languageFile.mapToPair(new PairFunction<String, String, String>() {
-            @Override
-            public Tuple2<String, String> call(String line) throws Exception {
-                String[] tokens = line.split(",");
-                return new Tuple2<String, String>(tokens[0], tokens[1]);
+            public Double call(Double d1, Double d2) throws Exception {
+                return Math.max(d1, d2);
             }
         });
-        return cityLanguagePairs;
-    }
 
-    private JavaPairRDD<String, String> selectCities(JavaRDD<String> cityFile) {
-        JavaPairRDD<String, String> cityCountryPairs = cityFile.mapToPair(new PairFunction<String, String, String>() {
+        JavaPairRDD<String, Double> minTempsByCity = minTemps.reduceByKey(new Function2<Double, Double, Double>() {
             @Override
-            public Tuple2<String, String> call(String line) throws Exception {
-                String[] tokens = line.split(",");
-                return new Tuple2<String, String>(tokens[1], tokens[0]);
+            public Double call(Double d1, Double d2) throws Exception {
+                return Math.min(d1, d2);
             }
         });
-        return cityCountryPairs;
+        JavaPairRDD<String, Tuple2<Double, Double>> cityTemps = maxTempsByCity.join(minTempsByCity);
+        cityTemps.saveAsTextFile(outputFile);
+    }
+
+    private JavaPairRDD<String, Double> selectCities(JavaRDD<String> cityTempsFile) {
+        return cityTempsFile.mapToPair(new PairFunction<String, String, Double>() {
+            @Override
+            public Tuple2<String, Double> call(String line) throws Exception {
+                String[] tokens = line.split(",");
+                return new Tuple2<String, Double>(tokens[0], Double.parseDouble(tokens[1]));
+            }
+        });
     }
 }
