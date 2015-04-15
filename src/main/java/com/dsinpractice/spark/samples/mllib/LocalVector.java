50b0e48b8902fafcb4f5bf7d87b7b4338f5b4dbf
Hemanth Yamijala <yhemanth@gmail.com>
2015-04-15 22:21:20 +0530

    Modified example to handle SparkVectors

--- src/main/java/com/dsinpractice/spark/samples/mllib/LocalVector.java
+++ src/main/java/com/dsinpractice/spark/samples/mllib/LocalVector.java
@@ -4,6 +4,7 @@ import com.google.common.collect.ContiguousSet;
 import com.google.common.collect.DiscreteDomain;
 import com.google.common.collect.Range;
 import com.google.common.primitives.Doubles;
+import org.apache.spark.mllib.linalg.SparseVector;
 import org.apache.spark.mllib.linalg.Vector;
 import org.apache.spark.mllib.linalg.Vectors;
 
@@ -24,14 +25,34 @@ public class LocalVector {
     private void run(String[] args) {
         if (args[0].equalsIgnoreCase("dense")) {
             makeDenseVector();
+        } else {
+            makeSparseVector();
         }
     }
 
+    private void makeSparseVector() {
+        double[] samples = getSamples();
+        int stepSize = 100;
+        int[] sparseIndices = new int[samples.length/stepSize];
+        double[] sparseSamples = new double[samples.length/stepSize];
+        for (int i = 0, j = 0; i < samples.length && j < sparseSamples.length; i+=stepSize, j++) {
+            sparseIndices[j] = i;
+            sparseSamples[j] = samples[i];
+        }
+        SparseVector sparseVector = (SparseVector)Vectors.sparse(samples.length, sparseIndices, sparseSamples);
+        System.out.println("Size of spare vector: " + sparseVector.size());
+        System.out.println("Size of indices: " + sparseVector.indices().length);
+    }
+
     private void makeDenseVector() {
-        ContiguousSet<Integer> numberSet = ContiguousSet.create(Range.closed(1, 1000000), DiscreteDomain.integers());
-        double[] samples = Doubles.toArray(numberSet);
+        double[] samples = getSamples();
 
         Vector v = Vectors.dense(samples);
-        System.out.println("Size of the vector: " + v.size());
+        System.out.println("Size of the dense vector: " + v.size());
+    }
+
+    private double[] getSamples() {
+        ContiguousSet<Integer> numberSet = ContiguousSet.create(Range.closed(1, 1000000), DiscreteDomain.integers());
+        return Doubles.toArray(numberSet);
     }
 }
